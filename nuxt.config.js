559ad052526f58ac615927a2f63e74f14f4bc68d
Mauro Mandracchia <mauro@insided.nl>
2017-06-05 19:03:21 +0200

    Remove ssr

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,5 +2,11 @@ module.exports = {
   cache: false,
   build: {
     publicPath: 'https://hackyourfuture.github.io/Iraq/'
-  }
+  },
+  generated: {
+    ssr: false
+  },
+  plugins: [
+    { src: '~components/SVGTriangles.vue', ssr: false }
+  ]
 }
